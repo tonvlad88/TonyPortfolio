08d3b379f183bad46759e0f0b071cc7dfc976f2d
Tony Saromines <tonysaromines@gmail.com>
2025-08-25 16:00:52 +0800

    Fix navbar background on scrolling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ export const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.screenY > 10);
+      setIsScrolled(window.scrollY > 10);
     };
 
     window.addEventListener("scroll", handleScroll);
